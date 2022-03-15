36a4c5101fed48359e5274b0946f9e06703b100a
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-15 08:23:17 -0300

    chore: add config for semantic imports

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,13 @@ module.exports = {
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  transformIgnorePatterns: ['./node_modules'],
+  modulePathIgnorePatterns: [
+    'src/data/protocols/*',
+    'src/data/usecases/*',
+    'src/domain/models/*',
+    'src/domain/usecases/*'
+  ],
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
