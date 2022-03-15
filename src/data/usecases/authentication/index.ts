401edd613b3a204bdd8a99ff8a77f49b4dd53f92
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-15 19:14:43 -0300

    feat: ensure RemoteAuthentication returns an AccountModel  on success

--- src/data/usecases/authentication/index.ts
+++ src/data/usecases/authentication/index.ts
@@ -2,23 +2,23 @@ import { InvalidCredentialsError } from '@domain/errors/invalida-credentials-err
 import { UnexpectedError } from '@domain/errors/unexpected-error'
 import { NotFoundError } from '@domain/errors/not-found-error'
 import { AccountModel } from '@domain/models/account-model'
-import { AuthenticationParams } from '@domain/usecases/authentication'
+import { Authentication, AuthenticationParams } from '@domain/usecases/authentication'
 import { HttpPostClient } from '@protocols/http/http-post-client'
 import { HttpStatusCode } from '@protocols/http/http-transform-response'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly HttpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.HttpPostClient.post({
       url: this.url,
       body: params
     })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
       case HttpStatusCode.notFound: throw new NotFoundError()
       default: throw new UnexpectedError()
