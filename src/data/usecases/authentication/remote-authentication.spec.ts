c8982ffe97bf964067571bbc6026038e95e71ec6
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-15 09:28:00 -0300

    test: ensure RemoteAuthentication calls HttpPostClient witch correct URL

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,17 +1,11 @@
 import { RemoteAuthentication } from '@data/usecases/authentication/remote-authentication'
-import { HttpPostClient } from '@data/protocols/http/http-post-client'
+import { HttpPostClientSpy } from '@data/test/mock-http-client'
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-      async post (url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
     const url = 'any_url'
     const httpPostClient = new HttpPostClientSpy()
+    // sut (System Under Test) => Objeto que esta sendo testado na vez
     const sut = new RemoteAuthentication(url, httpPostClient)
     await sut.auth()
     expect(httpPostClient.url).toBe(url)
