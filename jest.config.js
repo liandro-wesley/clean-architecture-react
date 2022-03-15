94ccef346fabcc60c338fec326ee96deaa4230b6
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-15 10:31:03 -0300

    refactor: remove absolute imports and move SUT creation to a factory method

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transformIgnorePatterns: ['./node_modules'],
-  modulePathIgnorePatterns: [
-    'src/data/protocols/*',
-    'src/data/usecases/*',
-    'src/domain/models/*',
-    'src/domain/usecases/*'
-  ],
+
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,26 @@
-import { RemoteAuthentication } from '@data/usecases/authentication/remote-authentication'
-import { HttpPostClientSpy } from '@data/test/mock-http-client'
+import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClientSpy } from '../../../data/test/mock-http-client'
+
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  // sut (System Under Test) => Objeto que esta sendo testado na vez
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    const url = 'any_url'
-    const httpPostClient = new HttpPostClientSpy()
-    // sut (System Under Test) => Objeto que esta sendo testado na vez
-    const sut = new RemoteAuthentication(url, httpPostClient)
+    const url = 'other_url'
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
-    expect(httpPostClient.url).toBe(url)
+    expect(httpPostClientSpy.url).toBe(url)
   })
 })
