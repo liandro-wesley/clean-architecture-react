510250d2e761b7bd2fff7c01f64b2053a1ceff5e
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-15 21:27:55 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/index.ts
+++ src/infra/http/axios-http-client/index.ts
@@ -1,8 +1,13 @@
-import { HttpPostClientParams } from '@data/protocols/http/http-post-client'
+import { HttpPostClientParams, HttpPostClient } from '@data/protocols/http/http-post-client'
+import { HttpTransformResponse } from '@protocols/http/http-transform-response'
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async post (params: HttpPostClientParams<any>): Promise<void> {
-    await axios.post(params.url, params.body)
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post (params: HttpPostClientParams<any>): Promise<HttpTransformResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
