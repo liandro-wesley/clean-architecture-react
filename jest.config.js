7b262d314bb8e2ef1f71ba58e862caca552d5878
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-16 23:41:20 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
@@ -11,7 +12,8 @@ module.exports = {
     '@protocols(.*)': '<rootDir>/src/data/protocols/$1',
     '@infra(.*)': '<rootDir>/src/infra/$1',
     '@domain/(.*)': '<rootDir>/src/domain/$1',
-    '@errors/(.*)': '<rootDir>/src/domain/errors/$1'
+    '@errors/(.*)': '<rootDir>/src/domain/errors/$1',
+    '\\.scss$': 'identity-obj-proxy' // returns a dummy {}
   },
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { CleanWebpackPlugin }  = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -10,14 +10,32 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@data': path.join(__dirname, './src/data'),
       '@domain': path.join(__dirname, './src/domain'),
       '@infra': path.join(__dirname, './src/infra'),
       '@protocols': path.join(__dirname, './src/data/protocols'),
-      '@erros': path.join(__dirname, './src/domain/errors'),
+      '@erros': path.join(__dirname, './src/domain/errors')
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { modules: true } },
+          { loader: 'sass-loader' }
+        ],
+        exclude: /node_modules/
+      }
+    ]
   },
   deveServer: {
     contentBase: './public',
