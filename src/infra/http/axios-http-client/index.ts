4f5b4062d8a5bd85b941ea386949edd5512c3283
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-17 18:26:07 -0300

    chore: update linter configs

--- src/infra/http/axios-http-client/index.ts
+++ src/infra/http/axios-http-client/index.ts
@@ -1,13 +1,18 @@
-import { HttpPostClientParams, HttpPostClient } from '@data/protocols/http/http-post-client'
-import { HttpTransformResponse } from '@protocols/http/http-transform-response'
-import axios from 'axios'
+import {
+  HttpPostClientParams,
+  HttpPostClient,
+} from "@data/protocols/http/http-post-client";
+import { HttpTransformResponse } from "@protocols/http/http-transform-response";
+import axios from "axios";
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
-  async post (params: HttpPostClientParams<any>): Promise<HttpTransformResponse<any>> {
-    const httpResponse = await axios.post(params.url, params.body)
+  async post(
+    params: HttpPostClientParams<any>
+  ): Promise<HttpTransformResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body);
     return {
       statusCode: httpResponse.status,
-      body: httpResponse.data
-    }
+      body: httpResponse.data,
+    };
   }
 }
---
+++ src/presentation/pages/login/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.scss";
+
+const Login: React.FC = () => {
+  return <div className={styles.login}>Hello</div>;
+};
+
+export default Login;
