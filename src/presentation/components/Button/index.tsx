dad99a04ee313d3b0509c0f2ffbed82895a9fb28
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-25 08:42:17 -0300

    fix: remove data-testid

--- src/presentation/components/Button/index.tsx
+++ src/presentation/components/Button/index.tsx
@@ -11,11 +11,7 @@ type ButtonProps = {
 
 const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
   return (
-    <button
-      data-testid="ButtonComponent"
-      {...props}
-      className={`${styles.button}`}
-    >
+    <button {...props} className={`${styles.button}`}>
       {!props.loading && props.body}
       {props.loading && (
         <SpinnerRoundFilled enabled={true} color={props.loadingcolor} />
