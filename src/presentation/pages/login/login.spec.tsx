3f1d07181e03889109569ceaa66b60b594beda9f
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-25 08:43:07 -0300

    test: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,12 +5,17 @@ import { render } from "@testing-library/react";
 describe("Login Component", () => {
   test("if isLoading is initially equal to false", () => {
     const { getByTestId } = render(<Login />);
-    const isLoading = getByTestId("ButtonComponent");
+    const isLoading = getByTestId("submit");
     expect(isLoading.childElementCount).toBe(0);
   });
   test("if errorMessage is initially equal to empty in LoginPage", () => {
-    const { getByRole, getByTestId } = render(<Login />);
+    const { getByRole } = render(<Login />);
     const errorMessage = getByRole("alertdialog");
     expect(errorMessage.innerHTML).toBe("");
+  });
+  test("if Button is initially disabled", () => {
+    const { getByTestId } = render(<Login />);
+    const submitButton = getByTestId("submit") as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(true);
   });
 });
