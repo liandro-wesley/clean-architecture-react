e30def039bc365be41f017c35a9df44aef096cad
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-25 16:22:29 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,31 +1,42 @@
 import React from "react";
 import Login from ".";
-import { render } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
+
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+  return {
+    sut,
+  };
+};
 
 describe("Login Component", () => {
   test("if isLoading is initially equal to false", () => {
-    const { getByTestId } = render(<Login />);
-    const isLoading = getByTestId("submit");
+    const { sut } = makeSut();
+    const isLoading = sut.getByTestId("submit");
     expect(isLoading.childElementCount).toBe(0);
   });
   test("if errorMessage is initially equal to empty in LoginPage", () => {
-    const { getByRole } = render(<Login />);
-    const errorMessage = getByRole("alertdialog");
+    const { sut } = makeSut();
+    const errorMessage = sut.getByRole("alertdialog");
     expect(errorMessage.innerHTML).toBe("");
   });
   test("if Button is initially disabled", () => {
-    const { getByTestId } = render(<Login />);
-    const submitButton = getByTestId("submit") as HTMLButtonElement;
+    const { sut } = makeSut();
+    const submitButton = sut.getByTestId("submit") as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
   });
   test("if initially errors in input of type email don't exist", () => {
-    const { getByTestId } = render(<Login />);
-    const emailInput = getByTestId("emailInput") as HTMLInputElement;
+    const { sut } = makeSut();
+    const emailInput = sut.getByTestId("emailInput") as HTMLInputElement;
     expect(emailInput.title).toBe("preencha o campo");
   });
   test("if initially errors in input of type password don't exist", () => {
-    const { getByTestId } = render(<Login />);
-    const passwordInput = getByTestId("passwordInput") as HTMLInputElement;
+    const { sut } = makeSut();
+    const passwordInput = sut.getByTestId("passwordInput") as HTMLInputElement;
     expect(passwordInput.title).toBe("preencha o campo");
   });
 });
