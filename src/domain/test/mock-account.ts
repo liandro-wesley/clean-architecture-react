784653c958cdce44e97dea8ebeae95da608e53e0
liandro-rosendo <liandro.silva1012@gmail.com>
2022-03-24 22:39:28 -0300

    feat: add new property to AccountModel

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,12 +1,13 @@
-import { AccountModel } from '@domain/models/account-model'
-import { AuthenticationParams } from '@domain/usecases/authentication'
-import faker from '@faker-js/faker'
+import { AccountModel } from "@domain/models/account-model";
+import { AuthenticationParams } from "@domain/usecases/authentication";
+import faker from "@faker-js/faker";
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
-  password: faker.internet.password()
-})
+  password: faker.internet.password(),
+});
 
 export const mockAuccountModel = (): AccountModel => ({
-  accessToken: faker.datatype.uuid()
-})
+  accessToken: faker.datatype.uuid(),
+  name: faker.internet.userName(),
+});
